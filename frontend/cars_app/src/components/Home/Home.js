c7b2b47e8fb30a274f80c745e4bea34ebef100ec
Миша Попов <misha@air-misa.home>
2023-06-26 17:53:20 -0400

    delete component carFormAdd. Relocate code to Home from carFormAdd

--- frontend/cars_app/src/components/Home/Home.js
+++ frontend/cars_app/src/components/Home/Home.js
@@ -1,9 +1,25 @@
 import s from './Home.module.css';
-import {CarAddForm} from '../CarAddForm/CarAddForm';
+import carList from '../../services/carList.service.js';
+import {useState, useEffect} from 'react';
 
-const Home = (props) => {
+const Home = () => {
+    const [make, setMake] = useState('');
+    const [price, setPrice] = useState('');
+    const [image, setImage] = useState('');
+    const [cars, setCars] = useState([]);
+
+    useEffect(async () => {
+        const result = carList().json();
+        setCars(result);
+    }, []);
+
     return (
-        <CarAddForm />
+        <section className={s.addCar}>
+            <input type="text" className={s.carMake} placeholder="Car make" />
+            <input type="text" className={s.carPrice} placeholder="Car price" />
+            <input type="text" className={s.carImage} placeholder="Car url" />
+            <button className={s.addButton}>Create</button>
+        </section>
     );
 }
 
