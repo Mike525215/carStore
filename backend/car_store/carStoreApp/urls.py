4de108aed3ad01fb312cdf1b73ef0217e0e71d9b
Миша Попов <misha@air-misa.home>
2023-06-26 14:16:59 -0400

    create APIView and also create path for it

--- backend/car_store/carStoreApp/urls.py
+++ backend/car_store/carStoreApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
+    path('cars/', CarsAPIView.as_view(), name='cars'),
+    path('cars/<int:pk>/', CarsAPIView.as_view(), name='car-detail')
 ]
--- backend/car_store/carStoreApp/views.py
+++ backend/car_store/carStoreApp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import *
+from .models import *
 
-# Create your views here.
+class CarsAPIView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        if kwargs.get('pk'):
+            queryset = Car.objects.get(id=kwargs.get('pk'))
+            serializer = CarSerializer(queryset)
+        else:
+            queryset = Car.objects.all()
+            serializer = CarSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        data = Car.objects.create(
+            make=request.data.get('make'),
+            price=request.data.get('price'),
+            image=request.data.get('image')
+        )
+        serializer = CarSerializer(request.data)
+        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        queryset = Car.objects.get(id=kwargs.get('pk'))
+        serializer = CarSerializer(data=request.data, instance=queryset)
+        serializer.is_valid()
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, *args, **kwargs):
+        car = Car.objects.filter(id=kwargs.get('pk'))
+        serializer = CarSerializer(car[0])
+        car.delete()
+        return Response(serializer.data)
+
