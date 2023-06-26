e7a9f701c3ec18531facc2f7e8425cddc1793796
Миша Попов <misha@air-misa.home>
2023-06-26 18:22:53 -0400

    add Cars on page

--- frontend/cars_app/src/components/Home/Home.js
+++ frontend/cars_app/src/components/Home/Home.js
@@ -1,25 +1,32 @@
 import s from './Home.module.css';
-import carList from '../../services/carList.service.js';
 import {useState, useEffect} from 'react';
+import {Cars} from '../Cars/Cars';
+import {services} from '../../services/carsServices.service.js';
 
 const Home = () => {
-    const [make, setMake] = useState('');
-    const [price, setPrice] = useState('');
-    const [image, setImage] = useState('');
     const [cars, setCars] = useState([]);
 
-    useEffect(async () => {
-        const result = carList().json();
+    const carsGet = async () => {
+        const request = await services.cars();
+        const result = await request.json();
         setCars(result);
+    }
+
+    useEffect(() => {
+        carsGet();
     }, []);
 
     return (
-        <section className={s.addCar}>
-            <input type="text" className={s.carMake} placeholder="Car make" />
-            <input type="text" className={s.carPrice} placeholder="Car price" />
-            <input type="text" className={s.carImage} placeholder="Car url" />
-            <button className={s.addButton}>Create</button>
-        </section>
+        <div>
+            <section className={s.addCar}>
+                <input type="text" className={s.carMake} placeholder="Car make" />
+                <input type="text" className={s.carPrice} placeholder="Car price" />
+                <input type="text" className={s.carImage} placeholder="Car url" />
+                <button className={s.addButton}>Create</button>
+            </section>
+
+            <Cars cars={cars} />
+        </div>
     );
 }
 
