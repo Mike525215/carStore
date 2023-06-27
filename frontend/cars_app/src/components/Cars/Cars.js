b919e250c033bc4d66591736913a4a69a926a6c4
Миша Попов <misha@air-misa.home>
2023-06-27 01:33:17 -0400

    change html code on Car component. Also remove styles for Car

--- frontend/cars_app/src/components/Cars/Cars.js
+++ frontend/cars_app/src/components/Cars/Cars.js
@@ -3,16 +3,7 @@ import s from './Cars.module.css';
 const Cars = (props) => {
     return (
         <div className={s.cars}>
-            {props.cars.map((car) => {
-                return (
-                    <div className={s.car}>
-                        <img className={s.carImage} src={car.image} alt="none"/>
-                        <p className={s.make}>Make: {car.make}</p>
-                        <p className={s.price}>Price: ${car.price}</p>
-                        <button className={s.readMore}>Read more</button>
-                    </div>
-                );
-            })}
+            {props.cars.map(car => <Car car={car} />)}
         </div>
     );
 }
