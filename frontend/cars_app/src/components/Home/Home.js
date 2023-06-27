93c478282060644631e774fc321ad05d5e632b62
Миша Попов <misha@air-misa.home>
2023-06-27 02:13:17 -0400

    include microservice. Also add states for inputs

--- frontend/cars_app/src/components/Home/Home.js
+++ frontend/cars_app/src/components/Home/Home.js
@@ -5,6 +5,9 @@ import {services} from '../../services/carsServices.service.js';
 
 const Home = () => {
     const [cars, setCars] = useState([]);
+    const [make, setMake] = useState('');
+    const [price, setPrice] = useState('');
+    const [image, setImage] = useState('');
 
     const carsGet = async () => {
         const request = await services.cars();
@@ -19,11 +22,24 @@ const Home = () => {
     return (
         <div>
             <section className={s.addCar}>
-                <input type="text" className={s.carMake} placeholder="Car make" />
-                <input type="text" className={s.carPrice} placeholder="Car price" />
-                <input type="text" className={s.carImage} placeholder="Car url" />
-                <button className={s.addButton}>Create</button>
+                <input type="text" className={s.carMake} placeholder="Car make"
+                       onChange={e => setMake(e.target.value)} value={make} />
+                <input type="text" className={s.carPrice} placeholder="Car price"
+                       onChange={e => setPrice(e.target.value)} value={price} />
+                <input type="text" className={s.carImage} placeholder="Car url"
+                       onChange={e => setImage(e.target.value)} value={image} />
+                <button className={s.addButton}
+                        onClick={
+                            async () => {
+                                await services.addCar(make, price, image);
+                                setMake('');
+                                setPrice('');
+                                setImage('');
+                                carsGet();
+                            }
+                        }>Create</button>
             </section>
+            <div className={s.title}><span>Cars catalog</span></div>
 
             <Cars cars={cars} />
         </div>
