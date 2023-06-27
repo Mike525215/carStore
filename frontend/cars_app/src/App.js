a447cbcb1842826c64fd28c75e372facc7beee21
Миша Попов <misha@air-misa.home>
2023-06-27 15:18:15 -0400

    include new component to App

--- frontend/cars_app/src/App.js
+++ frontend/cars_app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {Home} from './components/Home/Home';
 import {CarsRoutes} from './routes/routes';
+import {CarDetail} from './components/CarDetail/CarDetail';
 
 const App = () => {
     return (
         <>
-            <CarsRoutes element={Home} />
+            <CarsRoutes home={Home} detail={CarDetail} />
         </>
     );
 }
