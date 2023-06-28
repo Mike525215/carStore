f73ad7bc2c62fa22fee7a38d2ac6e09703298062
Миша Попов <misha@air-misa.home>
2023-06-28 19:37:27 -0400

    update code in views.py. Also create new urls for new views

--- backend/car_store/carStoreApp/urls.py
+++ backend/car_store/carStoreApp/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('cars/', CarsAPIView.as_view(), name='cars'),
-    path('cars/<int:pk>/', CarsAPIView.as_view(), name='car-detail')
+    path('cars/<int:pk>/', CarAPIView.as_view(), name='car-detail')
 ]
--- backend/car_store/carStoreApp/views.py
+++ backend/car_store/carStoreApp/views.py
@@ -1,36 +1,15 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, UpdateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
 from .models import *
 
-class CarsAPIView(APIView):
 
-    def get(self, request, *args, **kwargs):
-        if kwargs.get('pk'):
-            queryset = Car.objects.get(id=kwargs.get('pk'))
-            serializer = CarSerializer(queryset)
-        else:
-            queryset = Car.objects.all()
-            serializer = CarSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, *args, **kwargs):
-        serializer = CarSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def put(self, request, *args, **kwargs):
-        queryset = Car.objects.get(id=kwargs.get('pk'))
-        serializer = CarSerializer(data=request.data, instance=queryset)
-        serializer.is_valid()
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, *args, **kwargs):
-        car = Car.objects.filter(id=kwargs.get('pk'))
-        serializer = CarSerializer(car[0])
-        car.delete()
-        return Response(serializer.data)
+class CarsAPIView(ListAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+class CarAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
 
