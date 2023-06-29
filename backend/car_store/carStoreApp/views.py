1613088724f2add1568c97189ac55d011a36f27a
Миша Попов <misha@air-misa.home>
2023-06-28 23:37:39 -0400

    change ListAPIView on ListCreateAPIView. Also test new code

--- backend/car_store/carStoreApp/views.py
+++ backend/car_store/carStoreApp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, UpdateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
 from .models import *
 
 
-class CarsAPIView(ListAPIView):
+class CarsAPIView(ListCreateAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 class CarAPIView(RetrieveUpdateDestroyAPIView):
