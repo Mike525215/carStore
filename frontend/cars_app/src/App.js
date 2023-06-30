10932d5d77364211b96edb6ae5613a7d8dd8f90b
Миша Попов <misha@air-misa.home>
2023-06-30 02:02:56 -0400

    create new route for authorization. Also new route was forwarded into App component

--- frontend/cars_app/src/App.js
+++ frontend/cars_app/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import {Home} from './components/Home/Home';
 import {CarsRoutes} from './routes/routes';
 import {CarDetail} from './components/CarDetail/CarDetail';
+import {AuthComponent} from './components/AuthComponent/AuthComponent';
 
 const App = () => {
     return (
         <>
-            <CarsRoutes home={Home} detail={CarDetail} />
+            <CarsRoutes home={Home} detail={CarDetail} auth={AuthComponent}/>
         </>
     );
 }
--- frontend/cars_app/src/routes/routes.js
+++ frontend/cars_app/src/routes/routes.js
@@ -4,9 +4,10 @@ const CarsRoutes = (props) => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<props.home />} path='/cars/'/>
-                <Route element={<props.detail />} path='/cars/:id'/>
-                <Route element={<div>Page Not Found</div>} path='*'/>
+                <Route element={<props.home />} path='/cars/' />
+                <Route element={<props.detail />} path='/cars/:id' />
+                <Route element={<props.auth />} path='/auth/' />
+                <Route element={<div>Page Not Found</div>} path='*' />
             </Routes>
         </BrowserRouter>
     );
