b90cf44280e416e5c977422d4b4a79b7f1ce68af
Миша Попов <misha@air-misa.home>
2023-06-30 03:10:27 -0400

    create new microservice login. Also include it into AuthComponent

--- frontend/cars_app/src/components/AuthComponent/AuthComponent.js
+++ frontend/cars_app/src/components/AuthComponent/AuthComponent.js
@@ -1,48 +1,79 @@
 import s from './AuthComponent.module.css';
 import {useState} from 'react';
+import {Link} from 'react-router-dom';
 import {services} from '../../services/carsServices.service.js';
 
 const AuthComponent = () => {
+
     const [username, setName] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
     const [signUp, setStatus] = useState('false');
+    const [login, setLogin] = useState('false');
 
     let html = '';
-    if (signUp === 'false') {
+
+    if (login === 'true') {
         html =
-            <div className={s.authForm}>
-                <input type="text" placeholder="Username..." className={s.username}
-                       onChange={e => setName(e.target.value)} value={username} />
-                <input type="password" placeholder="Password..." className={s.password}
-                       onChange={e => setPassword(e.target.value)} value={password} />
-                <input type="text" placeholder="Email..." className={s.email}
-                       onChange={e => setEmail(e.target.value)} value={email} />
-                <button className={s.authBtn}
-                        onClick={
-                            async () => {
-                                const request = await services.auth(username, password, email);
-                                setName('');
-                                setPassword('');
-                                setEmail('');
-                                const result = await request.json();
-                                if (result.id) {
-                                    setStatus('true');
-                                } else {
-                                    alert('Form was filled incorrectly!');
-                                }
-                            }
-                        }>Authorization</button>
-            </div>
+            <>
+                <h2>Login was success! To move on home page click link below. </h2>
+                <Link to='/cars/'>Link</Link>
+            </>
     } else {
-        html =
-            <div className={s.authForm}>
-                <input type="text" placeholder="Username..." className={s.username}
-                       onChange={e => setName(e.target.value)} value={username} />
-                <input type="password" placeholder="Password..." className={s.password}
-                       onChange={e => setPassword(e.target.value)} value={password} />
-                <button className={s.authBtn}>Login</button>
-            </div>
+
+        if (signUp === 'false') {
+            html =
+                <div className={s.authForm}>
+                    <input type="text" placeholder="Username..." className={s.username}
+                           onChange={e => setName(e.target.value)} value={username} />
+                    <input type="password" placeholder="Password..." className={s.password}
+                           onChange={e => setPassword(e.target.value)} value={password} />
+                    <input type="text" placeholder="Email..." className={s.email}
+                           onChange={e => setEmail(e.target.value)} value={email} />
+                    <button className={s.authBtn}
+                            onClick={
+                                async () => {
+                                    const request = await services.auth(username, password, email);
+                                    setName('');
+                                    setPassword('');
+                                    setEmail('');
+                                    const result = await request.json();
+                                    if (result.id) {
+                                        setStatus('true');
+                                    } else {
+                                        if (result.username[0] === 'A user with that username already exists.') {
+                                            setStatus('true');
+                                        } else {
+                                            console.log(result);
+                                        }
+                                    }
+                                }
+                            }>Authorization</button>
+                </div>
+        } else {
+            html =
+                <div className={s.authForm}>
+                    <input type="text" placeholder="Username..." className={s.username}
+                           onChange={e => setName(e.target.value)} value={username} />
+                    <input type="password" placeholder="Password..." className={s.password}
+                           onChange={e => setPassword(e.target.value)} value={password} />
+                    <button className={s.authBtn}
+                            onClick={
+                                async () => {
+                                    const request = await services.login(username, password);
+                                    const result = await request.json();
+                                    setName('');
+                                    setPassword('');
+                                    console.log(result);
+                                    if (result.auth_token) {
+                                        setLogin('true');
+                                    } else {
+                                        console.log(result);
+                                    }
+                                }
+                            }>Login</button>
+                </div>
+        }
     }
     return html;
 };
--- frontend/cars_app/src/services/carsServices.service.js
+++ frontend/cars_app/src/services/carsServices.service.js
@@ -1,8 +1,10 @@
 const services = {
+
     async cars() {
         const request = await fetch('http://127.0.0.1:8000/api/v1/cars/');
         return request;
     },
+
     async addCar(make, price, image) {
         const data = new FormData();
         data.append('make', make);
@@ -13,10 +15,12 @@ const services = {
             body: data
         });
     },
+
     async getCar(id) {
         const request = await fetch('http://127.0.0.1:8000/api/v1/cars/' + id);
         return request;
     },
+
     async auth(username, password, email) {
         const data = new FormData();
         data.append('username', username);
@@ -27,7 +31,19 @@ const services = {
             body: data
         });
         return request;
+    },
+
+    async login(username, password) {
+        const data = new FormData();
+        data.append('username', username);
+        data.append('password', password);
+        const request = await fetch('http://127.0.0.1:8000/auth/token/login/', {
+            method: 'POST',
+            body: data
+        });
+        return request;
     }
+
 };
 
 export {services};
