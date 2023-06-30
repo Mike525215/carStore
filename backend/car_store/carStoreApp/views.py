0245ba0e458484dcf8735fae07776328a166242c
Миша Попов <misha@air-misa.home>
2023-06-30 17:57:18 -0400

    add permission class

--- backend/car_store/carStoreApp/views.py
+++ backend/car_store/carStoreApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
@@ -9,7 +10,10 @@ from .models import *
 class CarsAPIView(ListCreateAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+    permission_classes = [IsAuthenticated]
+
 class CarAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+
 
