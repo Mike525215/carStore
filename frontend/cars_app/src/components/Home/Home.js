ab5b795686b1a573eaf1006029128ee2474d861f
Миша Попов <misha@air-misa.home>
2023-06-30 17:56:56 -0400

    forwarding new props to components. Also add error message about authorization

--- frontend/cars_app/src/components/Home/Home.js
+++ frontend/cars_app/src/components/Home/Home.js
@@ -1,28 +1,40 @@
 import s from './Home.module.css';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, createContext} from 'react';
+import {Link} from 'react-router-dom';
 import {Cars} from '../Cars/Cars';
 import {services} from '../../services/carsServices.service.js';
 
-const Home = () => {
 
+const Home = (props) => {
     const [cars, setCars] = useState([]);
     const [make, setMake] = useState('');
     const [price, setPrice] = useState('');
     const [image, setImage] = useState('');
+    const [msg, setMsg] = useState('');
 
+    let html = '';
     const carsGet = async () => {
-        const request = await services.cars();
+        const request = await services.cars(props.token);
         const result = await request.json();
-        setCars(result);
+        if (result.detail) {
+            setMsg(result.detail);
+            console.log(props.token);
+        } else {
+            setCars(result);
+        }
     }
 
     useEffect(() => {
         carsGet();
     }, []);
 
+    if (msg) {
+        return <span>{msg}</span>;
+    } else {
+
     return (
         <div>
-            <section className={s.addCar}>
+           <section className={s.addCar}>
                 <input type="text" className={s.carMake} placeholder="Car make"
                        onChange={e => setMake(e.target.value)} value={make} />
                 <input type="text" className={s.carPrice} placeholder="Car price"
@@ -30,7 +42,7 @@ const Home = () => {
                 <input type="text" className={s.carImage} placeholder="Car url"
                        onChange={e => setImage(e.target.value)} value={image} />
                 <button className={s.addButton}
-                        onClick={
+                       onClick={
                             async () => {
                                 await services.addCar(make, price, image);
                                 setMake('');
@@ -45,6 +57,7 @@ const Home = () => {
             <Cars cars={cars} />
         </div>
     );
+    };
 }
 
 export {Home};
--- frontend/cars_app/src/routes/routes.js
+++ frontend/cars_app/src/routes/routes.js
@@ -1,12 +1,15 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {useState} from 'react';
 
 const CarsRoutes = (props) => {
+    const [token, setToken] = useState(JSON.parse(localStorage.getItem('token')) || '');
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<props.home />} path='/cars/' />
+                <Route element={<props.home token={token} />} path='/cars/' />
                 <Route element={<props.detail />} path='/cars/:id' />
-                <Route element={<props.auth />} path='/auth/' />
+                <Route element={<props.auth setToken={setToken} />} path='/auth/' />
                 <Route element={<div>Page Not Found</div>} path='*' />
             </Routes>
         </BrowserRouter>
