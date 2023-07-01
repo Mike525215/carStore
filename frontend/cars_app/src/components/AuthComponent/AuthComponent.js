637d699d023650888678136769321e2cd1af2145
Миша Попов <misha@air-misa.home>
2023-07-01 00:10:15 -0400

    include new components into app. Also add some features for auth-system

--- frontend/cars_app/src/components/AuthComponent/AuthComponent.js
+++ frontend/cars_app/src/components/AuthComponent/AuthComponent.js
@@ -1,7 +1,9 @@
 import s from './AuthComponent.module.css';
 import {useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import {services} from '../../services/carsServices.service.js';
+import {SignUp} from './SignUp';
+import {Login} from './Login';
 
 const AuthComponent = (props) => {
     const [username, setName] = useState('');
@@ -12,69 +14,16 @@ const AuthComponent = (props) => {
 
     let html = '';
 
-    if (login === 'true') {
-        html =
-            <>
-                <span className={s.text}>Login was success! To move on home page click link below.</span>
-                <Link to='/cars/' className={s.link}>Link</Link>
-            </>
+    if (signUp === 'false') {
+        html = <SignUp username={username} setName={setName}
+                       password={password} setPassword={setPassword}
+                       email={email} setEmail={setEmail}
+                       setStatus={setStatus} />
     } else {
-
-        if (signUp === 'false') {
-            html =
-                <div className={s.authForm}>
-                    <input type="text" placeholder="Username..." className={s.username}
-                           onChange={e => setName(e.target.value)} value={username} />
-                    <input type="password" placeholder="Password..." className={s.password}
-                           onChange={e => setPassword(e.target.value)} value={password} />
-                    <input type="text" placeholder="Email..." className={s.email}
-                           onChange={e => setEmail(e.target.value)} value={email} />
-                    <button className={s.authBtn}
-                            onClick={
-                                async () => {
-                                    const request = await services.auth(username, password, email);
-                                    setName('');
-                                    setPassword('');
-                                    setEmail('');
-                                    const result = await request.json();
-                                    if (result.id) {
-                                        setStatus('true');
-                                    } else {
-                                        if (result.username[0] === 'A user with that username already exists.') {
-                                            setStatus('true');
-                                        } else {
-                                            console.log(result);
-                                        }
-                                    }
-                                }
-                            }>Authorization</button>
-                </div>
-        } else {
-            html =
-                <div className={s.authForm}>
-                    <input type="text" placeholder="Username..." className={s.username}
-                           onChange={e => setName(e.target.value)} value={username} />
-                    <input type="password" placeholder="Password..." className={s.password}
-                           onChange={e => setPassword(e.target.value)} value={password} />
-                    <button className={s.authBtn}
-                            onClick={
-                                async () => {
-                                    const request = await services.login(username, password);
-                                    const result = await request.json();
-                                    setName('');
-                                    setPassword('');
-                                    console.log(result);
-                                    if (result.auth_token) {
-                                        setLogin('true');
-                                        localStorage.setItem('token', JSON.stringify(result.auth_token));
-                                    } else {
-                                        console.log(result);
-                                    }
-                                }
-                            }>Login</button>
-                </div>
-        }
+        html = <Login username={username} setName={setName}
+                      password={password} setPassword={setPassword} />
     }
+
     return html;
 };
 
