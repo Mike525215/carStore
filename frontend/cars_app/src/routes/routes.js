c67f9a129c2db626be484acd1dd271f2d0aa33fe
Миша Попов <misha@air-misa.home>
2023-07-01 00:07:37 -0400

    add props for route element. Also remove props for auth route

--- frontend/cars_app/src/routes/routes.js
+++ frontend/cars_app/src/routes/routes.js
@@ -2,14 +2,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {useState} from 'react';
 
 const CarsRoutes = (props) => {
-    const [token, setToken] = useState(JSON.parse(localStorage.getItem('token')) || '');
 
+    const token = JSON.parse(localStorage.getItem('token')) || '';
+    const username = JSON.parse(localStorage.getItem('username')) || '';
+    const password = JSON.parse(localStorage.getItem('password')) || '';
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<props.home token={token} />} path='/cars/' />
+                <Route element={<props.home token={token}
+                                            username={username}
+                                            password={password} />} path='/cars/' />
                 <Route element={<props.detail />} path='/cars/:id' />
-                <Route element={<props.auth setToken={setToken} />} path='/auth/' />
+                <Route element={<props.auth />} path='/auth/' />
                 <Route element={<div>Page Not Found</div>} path='*' />
             </Routes>
         </BrowserRouter>
