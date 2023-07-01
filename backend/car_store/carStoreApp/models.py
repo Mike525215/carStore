dfbb55112089205f6b34c8b386a5704ead7e9b98
Миша Попов <misha@air-misa.home>
2023-07-01 15:56:21 -0400

    update Car model

--- backend/car_store/carStoreApp/models.py
+++ backend/car_store/carStoreApp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Car(models.Model):
     make = models.CharField(max_length=100)
     price = models.FloatField(blank=True)
     image = models.URLField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
