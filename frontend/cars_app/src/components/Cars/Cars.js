1b6f1ae24cd0f8ccda977b4e427b66db933d84c4
Миша Попов <misha@air-misa.home>
2023-07-01 23:22:23 -0400

    forward token and carsGet method into Car component through Cars component

--- frontend/cars_app/src/components/Cars/Cars.js
+++ frontend/cars_app/src/components/Cars/Cars.js
@@ -3,7 +3,7 @@ import {Car} from './Car/Car';
 const Cars = (props) => {
     return (
         <div className={s.cars}>
-            {props.cars.map(car => <Car key={car.id} car={car} />)}
+            {props.cars.map(car => <Car key={car.id} car={car} token={props.token} carsGet={props.carsGet} />)}
         </div>
     );
 }
